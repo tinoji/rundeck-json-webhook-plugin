a7975967c9faf039e34ed1bc0bc2dc980f8673f6
tinoji <pot5.sunen.on.the.earth.614@gmail.com>
2017-10-09 19:46:55 +0900

    Add an example notification plugin from the official repository

---
+++ src/main/java/com/ketsuago/rundeck/plugins/huginn/HuginnNotificationPlugin.java
@@ -0,0 +1,26 @@
+package com.ketsuago.rundeck.plugins.huginn;
+
+import com.dtolabs.rundeck.plugins.notification.NotificationPlugin;
+import com.dtolabs.rundeck.core.plugins.Plugin;
+import com.dtolabs.rundeck.plugins.descriptions.PluginDescription;
+import com.dtolabs.rundeck.plugins.descriptions.PluginProperty;
+import java.util.*;
+
+@Plugin(service="Notification",name="example")
+@PluginDescription(title="Example Plugin", description="An example Plugin for Rundeck Notifications.")
+public class HuginnNotificationPlugin implements NotificationPlugin{
+
+    @PluginProperty(name = "example",title = "Example String",description = "Example description")
+    private String example;
+
+    public HuginnNotificationPlugin(){
+
+    }
+
+    public boolean postNotification(String trigger, Map executionData, Map config) {
+        System.err.printf("Trigger %s fired for %s, configuration: %s\n",trigger,executionData,config);
+        System.err.printf("Local field example is: %s\n",example);
+        return true;
+    }
+
+}
